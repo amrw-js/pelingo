a4f3e3122f1a41f78e15ff0647cf820900d29318
Amr Wagdy <wagdy@scaleflex.com>
2024-10-01 02:24:25 +0300

    chore: Init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='bg-red-700'>
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
@@ -13,7 +13,7 @@ function App() {
         </p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   )
 }
 
