45af37b6602440812a5f4cb457c65df1f7295f28
Amr Wagdy <wagdy@scaleflex.com>
2024-10-02 16:13:04 +0300

    fix: Build issues

--- src/helpers/adaptGbtTranslationResponse.ts
+++ src/helpers/adaptGbtTranslationResponse.ts
@@ -1,21 +1,13 @@
+import { ChatCompletion } from 'openai/resources/index.mjs'
+
 import { STRING_MATCH_REGEX, TEXT_MATCH_REGEX } from '../utils/presets'
 
-interface IChoice {
-  message: {
-    content: string
-  }
-}
-
-interface IGbtResponse {
-  choices: IChoice[]
-}
-
-export const adaptGbtTranslationResponse = (response: IGbtResponse) => {
+export const adaptGbtTranslationResponse = (response: ChatCompletion) => {
   const { choices = [] } = response
   const [{ message }] = choices
   const { content = '' } = message
-  const jsonMatch = content.match(TEXT_MATCH_REGEX)
-  const fallbackMatch = content.match(STRING_MATCH_REGEX)
+  const jsonMatch = content?.match(TEXT_MATCH_REGEX)
+  const fallbackMatch = content?.match(STRING_MATCH_REGEX)
 
   if (jsonMatch && jsonMatch[1]) return jsonMatch[1].replace(/\\n/g, '\n')
 
